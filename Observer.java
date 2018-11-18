43e95fca1457a31a274b4cb5ae9eb13d354c897a
Eric Boris <ericboris@Erics-MacBook-Air.local>
2018-11-18 12:52:15 -0800

    add subject, observer, and displayElement classes

---
+++ Observer.java
@@ -0,0 +1,12 @@
+/**
+ * Provide an Observer interface
+ *
+ * @author Eric Boris
+ * @version 11/18/2018
+ */
+public interface Observer {
+    /**
+     * update each observer
+     */
+    public void update();
+}
---
+++ Subject.java
@@ -0,0 +1,24 @@
+/**
+ * Provide a Subject interface
+ *
+ * @author Eric Boris
+ * @version 11/18/2018
+ */
+public interface Subject {
+    /**
+     * register a new observer
+     * 
+     * @param   observer        the new observer to register
+     */
+    public void register(Observer observer);
+    /**
+     * remove an existing observer
+     *
+     * @param   observer        the observer to remove
+     */
+    public void remove(Observer observer);
+    /**
+     * notify each existing observer of changes
+     */
+    public void notifyObservers();
+}
