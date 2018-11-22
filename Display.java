2bb1145957a22b4df263b63546c4964adc89030e
Eric Boris <the.eric.boris@gmail.com>
2018-11-21 18:05:00 -0800

    display implement Observer interface

---
+++ Display.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+/**
+ * handle displaying a fractal window and a settings window
+ *
+ * @author Eric Boris
+ * @version 11/7/18
+ */
+public class Display implements Observer{
+    public void update() {
+    
+    }
+}
